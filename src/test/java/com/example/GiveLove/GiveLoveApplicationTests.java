fcf9c3251b80b30f953aa7a7a63f61f97d1b9ec5
Hưng Nguyễn <hungamtp@gmail.com>
2021-09-13 04:20:32 +0700

    unit test userserice save()

--- src/test/java/com/example/GiveLove/GiveLoveApplicationTests.java
+++ src/test/java/com/example/GiveLove/GiveLoveApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.GiveLove;
 
+import com.example.GiveLove.entity.Users;
+import com.example.GiveLove.repository.UsersRepository;
+import com.example.GiveLove.services.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class GiveLoveApplicationTests {
 
+
+
 	@Test
 	void contextLoads() {
 	}
+
+
+
 
 }
